e78803a86bf92136751f8cd6de4d4b9e94406cdd
ryan montgomery <ryan.maciomaire@gmail.com>
2019-08-05 20:30:10 -0500

    ssl test

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ const HTTP_PORT = process.env.HTTP_PORT || 80,
   HTTPS_PORT = process.env.HTTPS_PORT || 443,
   PRIV_KEY = process.env.PRIVATEKEY || null,
   CERT = process.env.CERTIFICATE || null,
-  ROOT = process.env.ROOT_PATH || path.resolve(__dirname, 'dist')
+  ROOT = process.env.ROOT_PATH || path.resolve(__dirname, 'dist'),
+  CHALLENGE = process.env.CHALLENGE || null,
+  CERT_SECRET = process.env.CERT_SECRET || null
 
 const credentials = { key: PRIV_KEY, cert: CERT }
 const app = express()
 
 app.use(express.static(ROOT))
+if (CHALLENGE)
+  app.get(`.well-known/acme-challenge/${CHALLENGE}`, (req, res, next) =>
+    res.send(CERT_SECRET)
+  )
+
 app.get('*', (req, res, next) =>
   res.sendFile('./index.html', err => console.error)
 )
