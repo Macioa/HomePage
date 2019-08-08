cc9017146457d0144350a1a814857d658e4a6e3b
ryan montgomery <ryan.maciomaire@gmail.com>
2019-08-08 11:59:05 -0500

    install and configure jest

---
+++ ops/__tests__/hello.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { Hello } from '../../src/components/hello'
+
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4)
+  })
+})
+
+describe('Todo component renders the todo correctly', () => {
+  it('renders correctly', () => {
+    const rendered = renderer.create(
+      <Hello compiler='test/ts' framework='test/react' />
+    )
+    expect(rendered.toJSON()).toMatchSnapshot()
+  })
+})
