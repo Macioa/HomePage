4dcae50f4d02104daf51e9a2327c63715eecb5a4
ryan montgomery <ryan.maciomaire@gmail.com>
2019-08-11 15:25:25 -0500

    hello world

--- src/components/Three/index.tsx
+++ src/components/Three/index.tsx
@@ -1,9 +1,10 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useMemo } from 'react'
 import ReactDOM from 'react-dom'
 import * as THREE from 'three'
 import * as cannon from 'cannon'
 import './styles.css'
 import * as style from './styles.css'
+import { TextGeometry } from 'three'
 
 export interface Instance {
   world?: cannon.World
@@ -16,39 +17,48 @@ export interface Instance {
 
 export interface Obj {
   mesh: THREE.Mesh
-  body: cannon.Body
+  body?: cannon.Body
+  shape: string
+  startpos: number[]
+  mass: number
+  anchorgrav: number
 }
 
 // create necessary objects for scene
-export const init = ({
-  world = new CANNON.World(),
-  gravity = new CANNON.Vec3(0, 0, -9.82),
-  scene = new THREE.Scene(),
-  camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  ),
-  renderer = new THREE.WebGLRenderer()
-}: Instance) => {
-  world.gravity = new CANNON.Vec3(0, 0, -9.82)
+export const init = (
+  {
+    world = new CANNON.World(),
+    gravity = new CANNON.Vec3(0, 0, -9.82),
+    scene = new THREE.Scene(),
+    camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    ),
+    renderer = new THREE.WebGLRenderer()
+  }: Instance,
+  camerapos = [0, 0, 5]
+) => {
+  camera.position.set(camerapos[0], camerapos[1], camerapos[2])
+  world.gravity = gravity
   return { world, gravity, scene, camera, renderer }
 }
 
 // resize render and reproject aspect ratio for camera
-export const resize = (i: Instance, w: number, h: number) => {
+export const Resize = (i: Instance, w: number, h: number) => {
   i.renderer.setSize(w, h)
-  reproject(i, w, h)
+  Reproject(i, w, h)
 }
 
 // reproject asspect ratio for camera
-export const reproject = ({ camera }: Instance, w: number, h: number) => {
+export const Reproject = ({ camera }: Instance, w: number, h: number) => {
   camera.aspect = w / h
   camera.updateProjectionMatrix()
 }
 
-export const start = (i: Instance) => {
+// start render
+export const Start = (i: Instance) => {
   var prevRender: number
   const animate = (t = Date.now(), step = 1 / 60, maxSubSteps = 3) => {
     requestAnimationFrame(animate)
@@ -58,20 +68,106 @@ export const start = (i: Instance) => {
       i.world.step(step, dt, maxSubSteps)
     }
     prevRender = t
+    if (i.objects)
+      i.objects.forEach(o => {
+        // @ts-ignore
+        o.mesh.position.copy(o.body.position)
+        // @ts-ignore
+        o.mesh.setRotationFromQuaternion(o.body.quaternion)
+      })
   }
+
   animate()
 }
 
+// create geometry from string
+export const ComputeTextGeometry = (text = '', font: THREE.Font) => {
+  let mem = new Map()
+  return text.split('').map(c => {
+    let data = mem.get(c)
+      ? mem.get(c)
+      : new THREE.TextGeometry(c, {
+          size: 10,
+          height: 0.5,
+          curveSegments: 3,
+          font: font
+        })
+    if (!mem.get(c)) data.computeBoundingBox()
+    mem.set(c, data)
+    return data
+  })
+}
+
+// create meshes from geometry
+export const CreateMeshes = (
+  geos: THREE.Geometry[] = [],
+  material: THREE.Material
+) => geos.map(g => new THREE.Mesh(g, material))
+
+// add elements to simulation //compute bounds, find largest bound, create cannon body, add to THREE scene and CANNON sim
+export const AddToSim = (
+  i: Instance,
+  meshes: Obj[] = [],
+  defaultMass: number = 1000
+) => {
+  if (!i.objects) i.objects = []
+  return meshes.map(({ mesh, shape, startpos, mass }) => {
+    // mesh.geometry.computeBoundingBox()
+    let size = mesh.geometry.boundingBox.getSize(new THREE.Vector3()),
+      maxBound = size.x >= size.y ? size.x : size.y,
+      sp = startpos.length >= 3 ? startpos : [0, 0, 0]
+    maxBound = maxBound >= size.z ? maxBound : size.z
+    mass = mass ? mass : defaultMass
+    shape = shape.toLowerCase() === 'sphere' ? 'sphere' : 'box'
+    let cShape =
+        shape === 'sphere'
+          ? new cannon.Sphere(maxBound)
+          : new cannon.Box(new cannon.Vec3(size.x, size.y, size.z)),
+      body = new cannon.Body({
+        mass,
+        position: new cannon.Vec3(sp[0], sp[1], sp[2]),
+        shape: cShape
+      })
+    i.world.addBody(body)
+    i.scene.add(mesh)
+    let res = { mesh, body, shape, startpos, mass }
+    i.objects.push(res)
+    return res
+  })
+}
+
+//
+//
+//
+// create react element
 export const ThreeCanvas = () => {
   const ref = useRef(null)
-  const i = init({})
+  let i = init({ gravity: new cannon.Vec3(0, 0, 0) })
   const resolve = () =>
-    resize(i, ref.current.offsetWidth, ref.current.offsetHeight)
+    Resize(i, ref.current.offsetWidth, ref.current.offsetHeight)
   useEffect(() => {
     resolve()
     ref.current.appendChild(i.renderer.domElement)
     window.addEventListener('resize', resolve)
-    start(i)
+
+    AddToSim(
+      i,
+      CreateMeshes(
+        ComputeTextGeometry(
+          'Hello World',
+          new THREE.Font(require('./fontdata/moonlime.json'))
+        ),
+        new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+      ).map((m, i) => {
+        return {
+          mesh: m,
+          shape: 'box',
+          mass: 1,
+          startpos: [0, 0, 0]
+        }
+      })
+    )
+    Start(i)
   })
   return <div ref={ref} style={{ width: '100%', height: '100%' }} id='3Scene' />
 }
