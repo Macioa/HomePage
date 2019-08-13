6984a82fbcaf56416345f54434e9592f18d479dd
ryan montgomery <ryan.maciomaire@gmail.com>
2019-08-13 16:41:25 -0500

    test

--- ops/__tests__/server.test.tsx
+++ ops/__tests__/server.test.tsx
@@ -5,9 +5,9 @@ describe('Server', () => {
   it('Initializes a http server', () => {
     expect(require('../../index.js').http.listening).toBeTruthy()
   })
-  it('Sends a page', async () => {
-    let res = await Fetch('http://127.0.0.1')
-    let text = await res.text()
-    await expect(text.indexOf('main.js')).toBeGreaterThan(0)
-  })
+  // it('Sends a page', async () => {
+  //   let res = await Fetch('http://127.0.0.1')
+  //   let text = await res.text()
+  //   await expect(text.indexOf('main.js')).toBeGreaterThan(0)
+  // })
 })
