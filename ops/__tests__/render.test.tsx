fd355fc394aa1919ce2ca869c002bb5a67aa7030
ryan montgomery <ryan.maciomaire@gmail.com>
2019-08-28 15:02:56 -0500

    single browser session (improved mem usage)

--- ops/__tests__/render.test.tsx
+++ ops/__tests__/render.test.tsx
@@ -3,7 +3,7 @@ import { footerLinks } from '../../src/components/Footer'
 
 const puppeteer = require('puppeteer'),
   options = {
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    args: ['--disable - gpu', '--no-sandbox', '--disable-setuid-sandbox']
   }
 const path = require('path')
 const output = 'ops/__tests__/__snapshots__/'
@@ -15,37 +15,40 @@ deviceNames
 
 jest.setTimeout(30000)
 
+const browserPromise = (async () => {
+  return await puppeteer.launch(options)
+})()
+
 describe('Render Test', () => {
   it('standard', async () => {
-    let browser = await puppeteer.launch(options)
+    let browser = await Promise.resolve(browserPromise)
     let page = await browser.newPage()
-    browser.open
     await page.goto('file:' + path.resolve(__dirname, '../../dist/index.html'))
     await page.screenshot({ path: `${output}standard.png` })
-    await browser.close()
+    await page.close()
     await expect(page).toBeTruthy()
   })
   devices.forEach(async (d: any) => {
     it(`${d.name}`, async () => {
-      let browser = await puppeteer.launch(options)
+      let browser = await Promise.resolve(browserPromise)
       let page = await browser.newPage()
       await page.emulate(d)
       await page.goto(
         'file:' + path.resolve(__dirname, '../../dist/index.html')
       )
       await page.screenshot({ path: `${output}${d.name}.png` })
-      await browser.close()
+      await page.close()
       await expect(page).toBeTruthy()
     })
   })
 })
 
 describe('Link Test', () => {
-  let links = footerLinks.slice(0, footerLinks.length - 1)
+  let links = footerLinks.slice(0, footerLinks.length)
   links.forEach(async (link: any) => {
     it(`${link.props.id}`, async () => {
-      let browser = await puppeteer.launch(options),
-        page = await browser.newPage(),
+      let browser = await Promise.resolve(browserPromise)
+      let page = await browser.newPage(),
         screen
       await page.goto(
         'file:' + path.resolve(__dirname, '../../dist/index.html')
@@ -55,7 +58,7 @@ describe('Link Test', () => {
         async (p: any) => {
           screen = await p.screenshot({ path: `${output}${link.props.id}.png` })
           await expect(screen).toBeTruthy()
-          await browser.close()
+          await page.close()
         }
       )
     })
